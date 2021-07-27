3bea3c9bcb3d9479ea4f5f91df45a106bfbc03a9
Sumin Byeon <suminb@gmail.com>
2021-07-27 13:29:38 +0000

    APIException

--- coupang/partners/__init__.py
+++ coupang/partners/__init__.py
@@ -8,8 +8,14 @@ ACCESS_KEY = os.environ.get("COUPANG_ACCESS_KEY")
 SECRET_KEY = os.environ.get("COUPANG_SECRET_KEY")
 
 
+class APIException(Exception):
+    pass
+
+
 class APIResponse:
     def __init__(self, json_response):
-        self.response_code = json_response["rCode"]
+        self.response_code = int(json_response["rCode"])
         self.response_message = json_response["rMessage"]
+        if self.response_code != 0:
+            raise APIException(self.response_message)
         self.data = json_response["data"]
